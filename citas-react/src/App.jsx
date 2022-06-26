59a82c73986320a800f1ed98d06a48222325c763
Jorge-Sarabia <jorge@semantic.mx>
2022-06-26 13:35:59 -0500

    Citas React

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+// En local Storage solo se pueden guardar Strings
+
+function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS);
+    }
+    obtenerLS();
+  }, [])
+
+  // Usar useEffect para sincronizar el estado de la aplicación con el localStorage
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter(
+      (paciente) => paciente.id != id
+    );
+    setPacientes(pacientesActualizados);
+  };
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
