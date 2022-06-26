388f4b22fbad88ece9133068368384cfd8e5a272
Jorge-Sarabia <jorge@semantic.mx>
2022-06-26 14:38:21 -0500

    Crear form de presupuesto

--- control-gastos/src/components/NuevoPresupuesto.jsx
+++ control-gastos/src/components/NuevoPresupuesto.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
   return (
     <div className="contenedor-presupuesto sombra">
       <form action="" className="formulario">
@@ -10,6 +10,8 @@ const NuevoPresupuesto = () => {
             type="text"
             className="nuevo-presupuesto"
             placeholder="Añade tu presupuestos"
+            value={presupuesto}
+            onChange={e => setPresupuesto(e.target.value)}
           />
         </div>
 
