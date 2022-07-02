0c7de6f7147fb2b082d5a3e69174177a6cf5f794
Jorge-Sarabia <jorge@semantic.mx>
2022-07-02 18:43:44 -0500

    Validando el presupuesto

--- control-gastos/src/components/NuevoPresupuesto.jsx
+++ control-gastos/src/components/NuevoPresupuesto.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import { useState } from 'react';
+import Mensaje from './Mensaje';
 
 const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+  const [mensaje, setMensaje] = useState('');
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+    if (!Number(presupuesto) || Number(presupuesto) < 0) {
+      setMensaje('No es un presupuesto valido');
+    } else {
+      setMensaje('Si es un presupuesto valido');
+    }
+  };
+
   return (
-    <div className="contenedor-presupuesto sombra">
-      <form action="" className="formulario">
-        <div className="campo">
-          <label htmlFor="">Definir Presupuestos</label>
+    <div className='contenedor-presupuesto sombra'>
+      <form onSubmit={handlePresupuesto} className='formulario'>
+        <div className='campo'>
+          <label htmlFor=''>Definir Presupuestos</label>
           <input
-            type="text"
-            className="nuevo-presupuesto"
-            placeholder="Añade tu presupuestos"
+            type='text'
+            className='nuevo-presupuesto'
+            placeholder='Añade tu presupuestos'
             value={presupuesto}
-            onChange={e => setPresupuesto(e.target.value)}
+            onChange={(e) => setPresupuesto(e.target.value)}
           />
         </div>
 
-        <input type="submit" value="Añadir" />
+        <input type='submit' value='Añadir' />
+
+        {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
       </form>
     </div>
   );
